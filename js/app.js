383900b4afb7bfd8432fd0c288fbe247f748ee22
Jacob Dalton <jacob.dalton@sportngin.com>
2015-06-20 16:59:07 -0500

    move map creation out of function

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 var commentMarkers = [];
 
+var map = L.map('map').setView([45.001064, -93.256577], 13);
+
+// title
+L.tileLayer(
+  // 'http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png', // has bike paths in red, blue
+  'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; OpenCycleMap'
+  }
+).addTo(map);
+
 function buildMap(rides,comments) {
 
   pathPoints = rides[0].pathPoints; // since we only have one path at the moment
 
-  var map = L.map('map').setView([45.001064, -93.256577], 13);
-
-  // title
-  L.tileLayer(
-    // 'http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png', // has bike paths in red, blue
-    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    { 
-      attribution: '&copy; OpenCycleMap'
-    }
-  ).addTo(map);
 
   // path(s)
   var startTimeStr = pathPoints[0].time;
