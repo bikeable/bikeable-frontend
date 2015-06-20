24b398f60290c607af03c66aae29ef4e85fdeb24
Daniel Schulz-Jackson <daniel@10x.agency>
2015-06-20 14:16:52 -0500

    +comments file

---
+++ data/comments.js
@@ -0,0 +1,20 @@
+var comments = [
+	{
+		id="7483475329857239485732",
+		username: "bikemonkey",
+		happiness: -1,
+		content: "there is a pothole here."
+	},
+	{
+		id="43857948295938520938528",
+		username: "finrider",
+		happiness: 1,
+		content: "there is a pothole here."
+	},
+	{
+		id="43857948295938520938528",
+		username: "finrider",
+		happiness: 0,
+		content: "be sure to turn left if you want to stay on the greenway"
+	}
+];
